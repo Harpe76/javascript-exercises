25c95b06ccc2a75b64858cb77598c99c7759e05d
Harpe76 <garethhiggins@shaw.ca>
2023-01-04 20:26:59 -0700

    Completion of third assignment

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let stList = string.split('');
+    let finalString = "";
+    let i = 10;
+    for (i = stList.length - 1; i >= 0; i--) {
+        finalString += stList[i];
+    }
 
+    return finalString;
 };
 
 // Do not edit below this line
