bc3c8786576344d69720dc49436f8313577a2dec
Harpe76 <garethhiggins@shaw.ca>
2023-01-05 06:53:28 -0700

    Completion of the fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    
+    let difference;
+    let smallest;
 
+    if (first < 0 || second < 0 || !Number.isInteger(first) || !Number.isInteger(second)) {
+        return "ERROR";
+    }
+
+    if (second >= first) {
+        difference = second - first;
+        smallest = first;
+    } else {
+        difference = first - second;
+        smallest = second;
+    }
+    
+    let output = 0;
+    for (i = 0; i <= difference; i++) {
+        output += smallest;
+        smallest += 1;
+    }
+    return output;
 };
 
 // Do not edit below this line
